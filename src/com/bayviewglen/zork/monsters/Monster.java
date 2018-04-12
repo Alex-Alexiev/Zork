1babd59fabcadbf810a2f5606617a73ed6475976
joslee <joslee@jleesp3.bayviewglen.ca>
2018-04-12 15:39:57 -0400

    Added monsters and refactored monster package.

---
+++ src/com/bayviewglen/zork/monsters/Monster.java
@@ -0,0 +1,25 @@
+package com.bayviewglen.zork.monsters;
+
+import com.bayviewglen.zork.Player;
+
+abstract public class Monster {
+	private String id;
+	
+	private String description;
+	
+	//private 
+	
+	private int damage;
+	
+	private int health;
+	
+	public Monster(String id, String description, int damage, int health) {
+		this.id = id;
+		this.description = description;
+		this.damage = damage;
+		this.health = health;
+	}
+	
+	abstract public void ability(Player player);
+	
+}
---
+++ src/com/bayviewglen/zork/monsters/PlagueRat.java
@@ -0,0 +1,14 @@
+package com.bayviewglen.zork.monsters;
+
+import com.bayviewglen.zork.Player;
+
+public class PlagueRat extends Monster {
+	public PlagueRat(String id, String description, int damage, int health) {
+		super(id, description, damage, health);
+	}
+	
+	public void ability(Player player) {
+		player
+	}
+
+}
