446b298e221ba10e0f71ff3732c1f9e63c2413ca
mnam <mnam@mnamsp3.bayviewglen.ca>
2018-04-16 13:10:29 -0400

    Fix Merge

--- src/com/bayviewglen/zork/items/Food.java
+++ src/com/bayviewglen/zork/items/Food.java
@@ -1,5 +1,3 @@
-<<<<<<< Upstream, based on origin/master
-<<<<<<< Upstream, based on origin/master
 package com.bayviewglen.zork.items;
 
 import java.util.HashMap;
@@ -20,37 +18,6 @@ public class Food extends Item {
 		
 	public int getHealthPoints(String item) {
 		return health;
-=======
-package zork.items;
-=======
-package com.bayviewglen.zork.items;
-
-import java.util.HashMap;
->>>>>>> 9512bf8 Update Merge
-
-public class Food extends Item {
-	
-	/*
-	 * I removed the poison and generative properties 
-	 * because we can just put negative for poison 
-	 * and 0 or some other number for generative
-	 */
-	private int health;
-	
-	public Food(String id, int health) {
-		super(id, 1, true);
-		this.health = health;
-	}
-<<<<<<< Upstream, based on origin/master
-	
-	public int getHealthPoints() {
-		return healthPoints;
->>>>>>> a2f39d0 This is the basic setup of the weapons class and Evan resynchronized
-=======
-		
-	public int getHealthPoints(String item) {
-		return health;
->>>>>>> 9512bf8 Update Merge
 	}
 	
 }
--- src/com/bayviewglen/zork/items/Item.java
+++ src/com/bayviewglen/zork/items/Item.java
@@ -1,5 +1,3 @@
-<<<<<<< Upstream, based on origin/master
-<<<<<<< Upstream, based on origin/master
 package com.bayviewglen.zork.items;
 
 /* 
@@ -65,79 +63,6 @@ public class Item {
 		return ret;
 	}
 		
-=======
-package zork.items;
-=======
-package com.bayviewglen.zork.items;
->>>>>>> 9512bf8 Update Merge
-
-/* 
- * Item class that contains the following:
- *     - id that describes what it is (apple, sword, shield)
- *     - consumable that describes whether the item is consumable
- */
-
-public class Item {
-	
-	private String id;
-	private int amount = 0;
-	private boolean consumable;
-	
-	public Item(String id, int amount, boolean consumable) {
-		this.id = id;
-		this.amount = amount;
-		this.consumable = consumable;
-	}
-	
-	public Item() {}
-	
-	/*
-	 * Returns the item's id
-	 */
-	public String getId() {
-		return this.id;
-	}
-	
-	/*
-	 * Compares this items id with another's
-	 */
-	public boolean compareId(Item item) {
-		if (this.id.equals(item.getId())) {
-			return true;
-		}
-		return false;
-	}
-	
-	/*
-	 * Compares this items id with another id
-	 */
-	public boolean compareId(String id) {
-		if (this.id.equals(id)) {
-			return true;
-		}
-		return false;
-	}
-	
-<<<<<<< Upstream, based on origin/master
->>>>>>> a2f39d0 This is the basic setup of the weapons class and Evan resynchronized
-=======
-	public void addAmount(int amount) {
-		this.amount += amount;
-		if (this.amount < 0) this.amount = 0;
-	}
-	
-	public int getAmount() {
-		return amount;
-	}
-	
-	public String toString() {
-		String ret = ""+amount+" ";
-		if (amount > 1) ret += id+"'s";
-		else ret += id;
-		return ret;
-	}
-		
->>>>>>> 9512bf8 Update Merge
 	/*
 	 * Returns whether the item is consummable
 	 */
