08c71185f9f66c45385d9ad2e4744eb255258311
Alex Alexiev <aalexiev@bayviewglen.ca>
2018-05-02 13:15:58 -0400

    Added getMonsters method

--- src/com/bayviewglen/zork/entity/Entities.java
+++ src/com/bayviewglen/zork/entity/Entities.java
@@ -1,10 +1,29 @@
 package com.bayviewglen.zork.entity;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
-public class Entities {
+public abstract class Entities {
 	
 	private HashMap<String, Entity> entities = new HashMap<String, Entity>();
+	
+	public boolean hasMonsters() {
+		for (Entity e : entities.values()) {
+			if (e instanceof Monster) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public String toString() {
+		String ret = "Entities: ";
+		for (Entity e : entities.values()) {
+			ret+=e+", ";
+		}
+		return ret;
+	}
 	
 	public void addEntity(Entity entity) {
 		entities.put(entity.getId(), entity);
@@ -12,6 +31,19 @@ public class Entities {
 	
 	public Entity getWithId(String id) {
 		return entities.get(id);
+	}
+	
+	/*
+	 * This is just a temporary method for testing the attacking
+	 */
+	public List<Monster> getMonsters(int i) {
+		List<Monster> monsters = new ArrayList<Monster>();
+		for (Entity e : entities.values()) {
+			if (e instanceof Monster) {
+				monsters.add((Monster)e);
+			}
+		}
+		return monsters;
 	}
 
 }
