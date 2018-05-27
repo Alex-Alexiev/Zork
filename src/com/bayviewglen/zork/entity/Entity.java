d81a51e9b353f550f098ee012bb12f6fd0e414c9
mnam <mnam@mnamsp3.bayviewglen.ca>
2018-05-27 17:20:20 -0400

    Fixed up stuff with poison

--- src/com/bayviewglen/zork/entity/Entity.java
+++ src/com/bayviewglen/zork/entity/Entity.java
@@ -17,51 +17,77 @@ public abstract class Entity {
 		this.id = name.replaceAll("\\s", "").toLowerCase();
 	}
 
+	/*
+	 * Damage methods
+	 */
 	public abstract void attack(Entity e);
-
 	public void damage(int hitPoints) {
 		health -= hitPoints;
 	}
 
+	/*
+	 * Poison 
+	 */
 	public void addPoison(Poison poison) {
 		this.poisons.add(poison);
 	}
-
 	public void poison() {
-		for (Poison p : poisons) {
-			damage(p.damage());
+		int index = 0;
+		while (index < poisons.size()) {
+			Poison poison = poisons.get(index);
+			damage(poison.damage());
+			if (poison.isApplied()) {
+				index++;
+			} else {
+				poisons.remove(index);
+			}
 		}
 	}
-
+	
+	/*
+	 * Getters
+	 */
 	public String getId() {
 		return id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
-	public String toString() {
-		return name;
+	public int getHealth() {
+		return health;
 	}
-
+	public int getPoisonDamage() {
+		int poisonDamage = 0;
+		for (Poison poison : poisons) {
+			poisonDamage += poison.getDamage();
+		}
+		return poisonDamage;
+	}
+	public boolean isPoisoned() {
+		if (poisons.size() > 0) 
+			return true;
+		return false;
+	}
 	public boolean isStunned() {
 		return stunned;
 	}
-
+	
+	/*
+	 * Setters
+	 */
 	public void stun(boolean s) {
 		stunned = s;
-	}
-
-	public int getHealth() {
-		return health;
 	}
 
 	public void setHealth(int health) {
 		this.health = health;
 	}
 
-	public Poison getPoison() {
-		return null;
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString() {
+		return name;
 	}
 }
