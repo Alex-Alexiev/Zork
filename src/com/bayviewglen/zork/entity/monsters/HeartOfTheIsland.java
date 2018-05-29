a65fd9516e80bcb4a4d039b9aa5fc13a5189464f
joslee <joslee@jleesp3.bayviewglen.ca>
2018-05-29 15:04:17 -0400

    deleted extra map and added final boss

---
+++ src/com/bayviewglen/zork/entity/monsters/HeartOfTheIsland.java
@@ -0,0 +1,15 @@
+package com.bayviewglen.zork.entity.monsters;
+
+import com.bayviewglen.zork.entity.Entity;
+import com.bayviewglen.zork.entity.Monster;
+import com.bayviewglen.zork.entity.Player;
+
+public class HeartOfTheIsland extends Monster {
+
+	public HeartOfTheIsland() {
+		super("Heart Of The Island", "It sits at the very core of the cave. As it beats, Its tainted blood spurts onto the ground and is soaked up by the soil. It cries blood and echoes of infants crying can be heard when it is struck. There are bodies of people, too weak to make a noise, that are standing around it. All are armed.", 0,
+				0, 80000);
+	
+		
+	}
+}
